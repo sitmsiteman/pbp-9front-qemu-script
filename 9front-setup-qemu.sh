94a9cd1ac3eef7a4486f395eb8a95795336bd4cd
sitmsiteman <107298363+sitmsiteman@users.noreply.github.com>
2025-07-11 05:04:25 +0900

    Update 9front-setup-qemu.sh

--- 9front-setup-qemu.sh
+++ 9front-setup-qemu.sh
@@ -25,10 +25,10 @@ fi
 qemu-system-aarch64 -M virt-2.12,gic-version=3,accel=kvm \
 	-cpu host -m $RAM -smp $CORES \
 	-bios $BIOS \
+ 	-drive file=$DISK,if=none,id=disk \
+	-device virtio-blk-pci-non-transitional,drive=disk \
 	-drive file=$RELEASE.qcow2,if=none,id=installer \
 	-device virtio-blk-pci-non-transitional,drive=installer \
-	-drive file=$DISK,if=none,id=disk \
-	-device virtio-blk-pci-non-transitional,drive=disk \
 	-serial stdio \
 	-netdev user,hostfwd=tcp::567-:567,hostfwd=tcp::17019-:17019,id=net0 \
 	-device virtio-net-pci-non-transitional,netdev=net0
